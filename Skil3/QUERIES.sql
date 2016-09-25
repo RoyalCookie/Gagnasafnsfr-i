2243ef49c56be04521b83e01fa5284a9b10f48c8
Janus Þór Kristjánsson <Janus@Januss-MacBook-Pro.local>
2016-09-25 22:28:03 +0000

    First  Commit

---
+++ Skil3/QUERIES.sql
@@ -0,0 +1,34 @@
+
+
+CREATE TEMPORARY VIEW AllAccountRecords AS
+SELECT a.*, ar.rid, ar.rdate, ar.rtype, ar.ramount, ar.rbalance
+FROM accounts a
+LEFT JOIN accountrecords AS ar ON ar.aid = a.aid;
+
+
+
+CREATE TEMPORARY VIEW DebtorStatus AS
+SELECT DISTINCT p.pid, p.pname, (SELECT SUM(abalance) FROM accounts WHERE pid = p.pid)   
+FROM people p
+INNER JOIN accounts AS a ON p.pid = a.pid
+WHERE a.abalance < 0
+ORDER BY p.pid;
+
+
+
+SELECT * FROM DebtorStatus;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
