90528d97a8884ffb2794b2da162548952511eb3d
Tómas Helgi Stefánsson <tomhste@gmail.com>
2016-10-06 16:29:43 +0000

    Update QUERIES.sql

--- Skil3/QUERIES.sql
+++ Skil3/QUERIES.sql
@@ -23,12 +23,42 @@ FROM people p
 INNER JOIN accounts  AS a ON p.pid = a.pid
 ORDER BY p.pid;
 
-
-
-
-
 SELECT * FROM FinancialStatus;
 
+
+
+CREATE OR REPLACE FUNCTION  Billchecker () RETURNS TRIGGER AS $Billchecker$
+BEGIN
+IF(new.bamount < 0) THEN RAISE EXCEPTION 
+		    'The Bill is less then zero '
+		    USING ERRCODE = '45001';
+ELSE IF (new.bduedate <= CURRENT_DATE) THEN RAISE EXCEPTION 
+				       'Date is past current date'
+					USING ERRCODE = '45002';
+ELSE IF(TG_OP = 'DELETE')THEN RAISE EXCEPTION 
+		     'NOT ALLOUD TO DELET'
+		      USING ERRCODE = '45003';
+ELSE IF(TG_OP = 'UPDATE')THEN 
+			IF
+			(new.bid <> old.bid OR new.bduedate <> old.bduedate OR new.bamount <> old.bamount)	
+			THEN
+			RAISE EXCEPTION 
+			'NOT alloud to Update accept bispaid '
+			USING ERRCODE = '45004';
+			END if;
+		      
+END if;
+END if;
+END if;
+END if;
+return new;
+END;
+$Billchecker$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER CheckBills BEFORE INSERT OR UPDATE OR DELETE ON Bills
+FOR EACH ROW
+EXECUTE PROCEDURE Billchecker();
 
 
 
