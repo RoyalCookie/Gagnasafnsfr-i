73fe9f126ce8bb46127d67f326ff4077100ed454
Janus Þór Kristjánsson <Janus@Januss-MacBook-Pro.local>
2016-10-06 15:29:03 +0000

    Missing 3 in 3

--- As6/queries.sql
+++ As6/queries.sql
@@ -1,3 +1,4 @@
+/*
 CREATE VIEW PeopleAccountRecords AS
 SELECT P.pid, P.pname, P.pgender, P.pheight, AR.rid, AR.aid, AR.rdate, AR.rtype, AR.ramount, AR.rbalance, A.aover, A.abalance
 FROM people P, Accounts  A, AccountRecords AR
@@ -15,10 +16,24 @@ $$
 LANGUAGE sql;
 
 SELECT NEWACCOUNT(200);
+*/
+
+CREATE TRIGGER NoTransMidgets
+AFTER INSERT OR UPDATE OF pid ON people
+  FOR EACH ROW
+    IF (SELECT  P.pheight FROM people P) <= 0 THEN
+      RAISE EXCEPTION 'HEIGHT CANNOT BE NEGATIVE!'
+      USING ERRCODE = '45000'
+      SELECT 1;
+    ELSE
+      SELECT 2;
+    END IF;
 
 
-CREATE TRIGGER NOTRANSMIDGETS
-FOR EACH ROW
-WHEN((SELECT  P.pheight FROM people P)<= 0)
- RAISE EXCEPTION 'HEIGHT CANNOT BE NEGATIVE!'
- USING ERRCODE = '45000';   
+
+
+
+
+
+
+
